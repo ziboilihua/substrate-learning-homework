d0d79e1f709692a1c73276a55856a8693bda377d
zhouzibo <18208965365@163.com>
2021-03-30 11:31:09 +0800

    update π to rust f64 consts

--- lesson4/src/shape_area.rs
+++ lesson4/src/shape_area.rs
@@ -1,3 +1,4 @@
+use std::f64::consts::PI;
 pub enum Shape {
     Circle{ r: f64 },
     Rectangle{ width: f64, height: f64},
@@ -11,7 +12,7 @@ pub trait Area {
 impl Area for Shape {
     fn get_area(&self) -> f64 {
         match *self {
-            Shape::Circle{r} => r * r * 3.14,
+            Shape::Circle{r} => r * r * PI,
             Shape::Rectangle { width, height } => width * height,
             Shape::Triangle { a, b, c } => {
                 let p = (a + b + c) / 2.0;
